41f36446a38fc6c27f1213a44bfaa1cd88636d77
JesseSound <jesseearlemccormack@gmail.com>
2023-12-06 22:57:58 -0500

    Starting to add movement. Started with just defining different inputs based on object name.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,31 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class Movement: MonoBehaviour
 {
     string objectName;
+    KeyCode UP;
+    KeyCode DOWN;
+    KeyCode LEFT;
+    KeyCode RIGHT;
     // Start is called before the first frame update
     void Start()
     {
         //Get the name of the object the script is attached to. Hopefully, we will be able to condense movement
-         objectName = gameObject.name; 
+         objectName = gameObject.name;
+        //CONNOR I KNOW YOU HATE STRINGS BUT I DON'T OK AT LEAST NOT IN THIS CONTEXT
+        if (objectName == "Capsule")
+        {
+            UP = KeyCode.W;
+            DOWN = KeyCode.S;
+            RIGHT = KeyCode.D;
+            LEFT = KeyCode.A;
+        }
+        else if (objectName == "Square")
+        {
+            UP = KeyCode.I;
+            DOWN = KeyCode.K;
+            RIGHT = KeyCode.L;
+            LEFT = KeyCode.J;
+        }
+        else if (objectName == "Circle")
+        {
+            UP = KeyCode.T;
+            DOWN = KeyCode.G;
+            RIGHT = KeyCode.H;
+            LEFT = KeyCode.F;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         
-        if(objectName == "Capsule")
-        {
 
-        } else if (objectName == "Square")
+        if (Input.GetKeyDown(UP))
         {
-
-        }else if(objectName == "Circle")
-        {
-
+            Debug.Log("UP!");
         }
-
-
+        else if (Input.GetKeyDown(DOWN))
+        {
+            Debug.Log("DOWN!");
+        }
+        else if (Input.GetKeyDown(LEFT))
+        {
+            Debug.Log("LEFT!");
+        }
+        else if (Input.GetKeyDown(RIGHT))
+        {
+            Debug.Log("RIGHT!");
+        }
     }
 }
